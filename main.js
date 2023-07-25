a60998c01d32f0ff3ae7c90065ae2a451a1c57d2
aryanjalali <aryanjalalicoding@gmail.com>
2023-07-25 14:45:08 -0400

    Attach functionality to HTML buttons
    Made js functions and attached them to buttons for resizing the
    etch-a-sketch, changing to rainbow etch, and changing to black etch.
    This allows the user to customize their etch-a-sketch experience and
    means this etch-a-sketch project has all of it's major functions. The
    js side of things is mostly completed, although the code to reset the
    etch-a-sketch is repeated several times and may be created into its
    own function.

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-const etchSketch = document.querySelector("#etch-sketch");
+const etchSketch = document.querySelector('#etch-sketch');
+const blackButton = document.querySelector('#black-btn');
+const rainbowButton = document.querySelector('#rainbow-btn');
+const gridSizeButton = document.querySelector('#size-submit');
+let etchState = 'black'; // Keeps track of whether the etchsketch is black or rainbow
+let gridSize = 16; 
 
 etchSketch.addEventListener('mouseover', blackColorEtch);
 etchSketch.addEventListener('mousedown', blackColorEtch);
 
-fillEtch(16);
+blackButton.addEventListener('click', changeToBlack);
+rainbowButton.addEventListener('click', changeToRainbow);
+gridSizeButton.addEventListener('click', resizeEtch);
 
-function fillEtch (gridSize, blackness) {
+fillEtch(gridSize);
+
+function fillEtch (size, blackness) {
      // Fill the etch-a-sketch with a grid
-     for (let i = 0; i<gridSize; i++) {
+     for (let i = 0; i<size; i++) {
           let rowDiv = document.createElement('div');
           rowDiv.className = 'row-grid';
-          for (let i = 0; i<gridSize; i++) {
+          for (let i = 0; i<size; i++) {
                let columnDiv = document.createElement('div');
                columnDiv.className = 'square';
                columnDiv.blackness = 0;
@@ -39,5 +48,60 @@ function blackColorEtch (e) {
      if (e.target.id != 'etch-sketch' && e.buttons === 1 && blackness != 0){
           e.target.style.backgroundColor = `rgb(${blackness}, ${blackness}, ${blackness})`;
           e.target.blackness += 25;
+     };
+}
+
+function changeToBlack (e) {
+     if (etchState === 'rainbow'){
+          etchState = 'black';
+          etchSketch.removeEventListener('mouseover', randomColorEtch);
+          etchSketch.removeEventListener('mousedown', randomColorEtch);
+
+          etchSketch.addEventListener('mouseover', blackColorEtch);
+          etchSketch.addEventListener('mousedown', blackColorEtch);
+
+          while (etchSketch.hasChildNodes()) {
+               etchSketch.removeChild(etchSketch.firstChild);
+          };
+          fillEtch(gridSize);
+     };     
+}
+
+function changeToRainbow (e) {
+     if (etchState === 'black'){
+          etchState = 'rainbow';
+          etchSketch.removeEventListener('mouseover', blackColorEtch);
+          etchSketch.removeEventListener('mousedown', blackColorEtch);
+
+          etchSketch.addEventListener('mouseover', randomColorEtch);
+          etchSketch.addEventListener('mousedown', randomColorEtch);
+
+          while (etchSketch.hasChildNodes()) {
+               etchSketch.removeChild(etchSketch.firstChild);
+          };
+          fillEtch(gridSize);
+     };     
+}
+
+function resizeEtch (e){
+     e.preventDefault();
+
+     let userSize = Number(`${document.querySelector('#size-input').value}`);
+     if (Number.isInteger(userSize) && userSize >= 4 && userSize <= 100) {
+          gridSize = userSize;
+          
+          while (etchSketch.hasChildNodes()) {
+               etchSketch.removeChild(etchSketch.firstChild);
+          };
+          fillEtch(gridSize);
+     } else {
+          let errorMessageArea = document.querySelector('#error-msg');
+
+          errorMessageArea.classList.add('error');
+          errorMessageArea.innerHTML = 'Please enter integer between 4 and 100 (inclusive)';
+          setTimeout(() => {
+               errorMessageArea.innerHTML = '';
+               errorMessageArea.classList.remove('error');
+          }, 10000);
      };
 }
