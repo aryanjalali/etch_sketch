71a21fb403471ac2a661b3dcb585213cd8cc58e9
aryanjalali <aryanjalalicoding@gmail.com>
2023-07-25 13:46:56 -0400

    Add black color shading to etch-a-sketch
    Created new function that allows the user to progressively shade each
    individual etch-a-sketch square, going from white to black in 10 steps.
    The function works alone but is not yet connected to the buttons. Next
    steps should include adding functionality to all the HTML buttons.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const etchSketch = document.querySelector("#etch-sketch");
 
-etchSketch.addEventListener('mouseover', randomColorEtch);
-etchSketch.addEventListener('mousedown', randomColorEtch);
+etchSketch.addEventListener('mouseover', blackColorEtch);
+etchSketch.addEventListener('mousedown', blackColorEtch);
 
-fillEtch(48);
+fillEtch(16);
 
-function fillEtch (gridSize) {
+function fillEtch (gridSize, blackness) {
      // Fill the etch-a-sketch with a grid
      for (let i = 0; i<gridSize; i++) {
           let rowDiv = document.createElement('div');
@@ -13,6 +13,7 @@ function fillEtch (gridSize) {
           for (let i = 0; i<gridSize; i++) {
                let columnDiv = document.createElement('div');
                columnDiv.className = 'square';
+               columnDiv.blackness = 0;
                rowDiv.appendChild(columnDiv);
           }
           etchSketch.appendChild(rowDiv);
@@ -25,7 +26,18 @@ function randomColorEtch (e) {
 
      // Generate random numbers and use them for background RGB colors
      let rgb = new Array(Math.floor(Math.random()*256), Math.floor(Math.random()*256), Math.floor(Math.random()*256));
+     // First condition ensures the entire etch-sketch is not colored at once
      if (e.target.id != 'etch-sketch' && e.buttons === 1){
           e.target.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
-     }
+     };
+}
+
+function blackColorEtch (e) {
+     e.preventDefault();
+
+     let blackness = 250 - e.target.blackness;
+     if (e.target.id != 'etch-sketch' && e.buttons === 1 && blackness != 0){
+          e.target.style.backgroundColor = `rgb(${blackness}, ${blackness}, ${blackness})`;
+          e.target.blackness += 25;
+     };
 }
